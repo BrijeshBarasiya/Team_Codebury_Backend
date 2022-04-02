f23e38e06aaf73f063cac33480d712e084576e8a
Parth Darji <parth.darji@simformsolutions.com>
2022-04-02 16:00:20 +0530

    Login Api

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'fname' => "parth",
+            'lname' => "darji",
+            'email' => "parthdarji1912@gmail.com",
+            'password' => Hash::make('password'),
+            'gender' => "male",
+            'department' => "opensource",
+            'job' => "trainee",
+            'image_url'=>"soon",
+            'dob'=>"24.04.2021"
+
+        ]);
+    }
+}
